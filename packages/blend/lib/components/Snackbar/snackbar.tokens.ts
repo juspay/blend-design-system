1d7f5fcf41aa9c507af2c6d596c21b446d24db6e
Aryan <aryan.pidiha@juspay.in>
2025-08-08 17:16:32 +0530

    fix: restructured tokens for snackbar

--- packages/blend/lib/components/Snackbar/snackbar.tokens.ts
+++ packages/blend/lib/components/Snackbar/snackbar.tokens.ts
@@ -1,103 +1,180 @@
 import type { CSSObject } from 'styled-components'
-import { FOUNDATION_THEME } from '../../tokens'
-import type { VariantType } from '../Text/Text'
+import { BreakpointType } from '../../breakpoints/breakPoints'
+import { FoundationTokenType } from '../../tokens/theme.token'
+import { SnackbarVariant } from './types'
 
 export type SnackbarTokens = Readonly<{
-    icon: {
-        color: {
-            info: CSSObject['color']
-            success: CSSObject['color']
-            warning: CSSObject['color']
-            error: CSSObject['color']
+    backgroundColor: CSSObject['color']
+    borderRadius: CSSObject['borderRadius']
+    padding: CSSObject['padding']
+    minWidth: CSSObject['minWidth']
+    maxWidth: CSSObject['maxWidth']
+    boxShadow: CSSObject['boxShadow']
+    gap: CSSObject['gap']
+    content: {
+        gap: CSSObject['gap']
+        infoIcon: {
+            color: {
+                [key in SnackbarVariant]: CSSObject['color']
+            }
+            size: {
+                [key in SnackbarVariant]: CSSObject['fontSize']
+            }
         }
-    }
-    container: {
-        backgroundColor: CSSObject['color']
-        borderRadius: CSSObject['borderRadius']
-        padding: CSSObject['padding']
-        minWidth: CSSObject['minWidth']
-        maxWidth: CSSObject['maxWidth']
-        boxShadow: CSSObject['boxShadow']
-    }
-    header: {
-        layout: {
+        container: {
             gap: CSSObject['gap']
-            marginBottom: CSSObject['marginBottom']
-            iconGap: CSSObject['gap']
-        }
-        text: {
-            color: CSSObject['color']
-            variant: VariantType
+            textContainer: {
+                gap: CSSObject['gap']
+                header: {
+                    color: CSSObject['color']
+                    fontSize: CSSObject['fontSize']
+                    fontWeight: CSSObject['fontWeight']
+                }
+                description: {
+                    color: CSSObject['color']
+                    fontSize: CSSObject['fontSize']
+                    fontWeight: CSSObject['fontWeight']
+                }
+            }
+            actionButton: {
+                layout: {
+                    paddingX: CSSObject['padding']
+                }
+                text: {
+                    color: CSSObject['color']
+                    fontSize: CSSObject['fontSize']
+                    fontWeight: CSSObject['fontWeight']
+                }
+            }
         }
     }
-    description: {
-        layout: {
-            paddingLeft: CSSObject['paddingLeft']
-            gap: CSSObject['gap']
-        }
-        text: {
-            color: CSSObject['color']
-            variant: VariantType
-        }
-    }
-    actionButton: {
-        layout: {
-            paddingX: CSSObject['padding']
-        }
-        text: {
-            color: CSSObject['color']
-            variant: VariantType
-        }
+
+    crossIcon: {
+        size: CSSObject['fontSize']
+        color: CSSObject['color']
     }
 }>
 
-const snackbarTokens: SnackbarTokens = {
-    icon: {
-        color: {
-            info: FOUNDATION_THEME.colors.primary[300],
-            success: FOUNDATION_THEME.colors.green[500],
-            warning: FOUNDATION_THEME.colors.yellow[500],
-            error: FOUNDATION_THEME.colors.red[500],
-        },
-    },
-    container: {
-        backgroundColor: FOUNDATION_THEME.colors.gray[900],
-        borderRadius: FOUNDATION_THEME.border.radius[8],
-        padding: FOUNDATION_THEME.unit[16],
-        minWidth: 400,
-        maxWidth: 500,
-        boxShadow: FOUNDATION_THEME.shadows.lg,
-    },
-    header: {
-        layout: {
-            gap: FOUNDATION_THEME.unit[8],
-            marginBottom: FOUNDATION_THEME.unit[8],
-            iconGap: FOUNDATION_THEME.unit[8],
-        },
-        text: {
-            color: FOUNDATION_THEME.colors.gray[0],
-            variant: 'body.md',
-        },
-    },
-    description: {
-        layout: {
-            paddingLeft: FOUNDATION_THEME.unit[24],
-            gap: FOUNDATION_THEME.unit[18],
-        },
-        text: {
-            color: FOUNDATION_THEME.colors.gray[300],
-            variant: 'body.md',
-        },
-    },
-    actionButton: {
-        layout: {
-            paddingX: FOUNDATION_THEME.unit[2],
-        },
-        text: {
-            color: FOUNDATION_THEME.colors.gray[100],
-            variant: 'body.md',
-        },
-    },
+export type ResponsiveSnackbarTokens = {
+    [key in keyof BreakpointType]: SnackbarTokens
 }
 
-export default snackbarTokens
+export const getSnackbarTokens = (
+    foundationToken: FoundationTokenType
+): ResponsiveSnackbarTokens => {
+    return {
+        sm: {
+            backgroundColor: foundationToken.colors.gray[900],
+            borderRadius: foundationToken.border.radius[12],
+            padding: foundationToken.unit[16],
+            minWidth: 320,
+            maxWidth: 350,
+            boxShadow: foundationToken.shadows.lg,
+            gap: foundationToken.unit[10],
+            content: {
+                gap: foundationToken.unit[10],
+                infoIcon: {
+                    color: {
+                        info: foundationToken.colors.primary[300],
+                        success: foundationToken.colors.green[500],
+                        warning: foundationToken.colors.yellow[500],
+                        error: foundationToken.colors.red[500],
+                    },
+                    size: {
+                        info: foundationToken.unit[16],
+                        success: foundationToken.unit[16],
+                        warning: foundationToken.unit[16],
+                        error: foundationToken.unit[16],
+                    },
+                },
+                container: {
+                    gap: foundationToken.unit[14],
+                    textContainer: {
+                        gap: foundationToken.unit[6],
+                        header: {
+                            color: foundationToken.colors.gray[25],
+                            fontSize: 14,
+                            fontWeight: foundationToken.font.weight[500],
+                        },
+                        description: {
+                            color: foundationToken.colors.gray[300],
+                            fontSize: 14,
+                            fontWeight: foundationToken.font.weight[400],
+                        },
+                    },
+                    actionButton: {
+                        layout: {
+                            paddingX: foundationToken.unit[2],
+                        },
+                        text: {
+                            color: foundationToken.colors.gray[100],
+                            fontSize: 14,
+                            fontWeight: foundationToken.font.weight[600],
+                        },
+                    },
+                },
+            },
+            crossIcon: {
+                size: 16,
+                color: foundationToken.colors.gray[0],
+            },
+        },
+        lg: {
+            backgroundColor: foundationToken.colors.gray[900],
+            borderRadius: foundationToken.border.radius[8],
+            padding: foundationToken.unit[16],
+            minWidth: 400,
+            maxWidth: 420,
+            boxShadow: foundationToken.shadows.lg,
+            gap: foundationToken.unit[10],
+            content: {
+                gap: foundationToken.unit[8],
+                infoIcon: {
+                    color: {
+                        info: foundationToken.colors.primary[300],
+                        success: foundationToken.colors.green[500],
+                        warning: foundationToken.colors.yellow[500],
+                        error: foundationToken.colors.red[500],
+                    },
+                    size: {
+                        info: foundationToken.unit[16],
+                        success: foundationToken.unit[16],
+                        warning: foundationToken.unit[16],
+                        error: foundationToken.unit[16],
+                    },
+                },
+                container: {
+                    gap: foundationToken.unit[18],
+                    textContainer: {
+                        gap: foundationToken.unit[6],
+                        header: {
+                            color: foundationToken.colors.gray[25],
+                            fontSize: 16,
+                            fontWeight: foundationToken.font.weight[600],
+                        },
+                        description: {
+                            color: foundationToken.colors.gray[300],
+                            fontSize: 14,
+                            fontWeight: foundationToken.font.weight[400],
+                        },
+                    },
+                    actionButton: {
+                        layout: {
+                            paddingX: foundationToken.unit[2],
+                        },
+                        text: {
+                            color: foundationToken.colors.gray[100],
+                            fontSize: 14,
+                            fontWeight: foundationToken.font.weight[600],
+                        },
+                    },
+                },
+            },
+
+            crossIcon: {
+                size: 16,
+                color: foundationToken.colors.gray[0],
+            },
+        },
+    }
+}
