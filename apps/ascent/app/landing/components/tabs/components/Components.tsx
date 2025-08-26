d12797678a27a9c7659e785018b571a5a6342105
Shristi Gupta <shristi.gupta@Shristi-Gupta-D624CC6QDG.local>
2025-08-26 16:45:58 +0530

    fix: unwanted white divider removed

--- apps/ascent/app/landing/components/tabs/components/Components.tsx
+++ apps/ascent/app/landing/components/tabs/components/Components.tsx
@@ -5,7 +5,7 @@ import { GradientHeading } from '../../GradientHeading'
 
 export const Components = () => {
     return (
-        <div className=" border-b w-full mb-50 ">
+        <div className="w-full mb-50 ">
             <GradientHeading text="Components" classes="justify-center" />
             <div className="lg:mt-50 md:mt-25 sm:mt-18 mt-15 flex items-center justify-center flex-col gap-14">
                 <div className="text-[var(--intro-text-color)] 2xl:text-4xl xl:text-3xl lg:text-2xl md:text-lg sm:text-xs text-[length:var(--text-xxs)] opacity-80 font-light text-center">
