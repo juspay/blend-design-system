d185ad0f9b104c1b122c570dd766560d83601ef1
12-aryan <aryan.pidiha@juspay.in>
2025-09-01 19:47:59 +0530

    feat: props to handle timeZone and hours format (#330)
    * feat: props to handle timeZone and hours format
    
    * feat: add GranularChartsDemo component for customizable dashboard
    
    * fix: intervals
    
    * fix: changed interface from type

--- packages/blend/lib/components/Charts/index.ts
+++ packages/blend/lib/components/Charts/index.ts
@@ -1,2 +1,8 @@
 export { default as Charts } from './Charts'
+
+export { default as CoreChart } from './CoreChart'
+export { default as ChartContainer } from './ChartContainer'
+export { default as ChartHeader } from './ChartHeader'
+export { default as ChartLegends } from './ChartLegend'
+
 export * from './types'
