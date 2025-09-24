42a64452fcfa61ea816e397d9e14787dc6bd0f75
Vinit Khandal <vinit.khandal@juspay.in>
2025-09-24 17:42:42 +0530

    refactor: update Breadcrumb component tokens

--- apps/site/src/demos/SidebarDemo.tsx
+++ apps/site/src/demos/SidebarDemo.tsx
@@ -134,7 +134,7 @@ const SidebarDemo = () => {
         | 'keyValuePair'
         | 'card'
         | 'dataRangePicker'
-    >('snackbar')
+    >('breadcrumb')
 
     const [activeTenant, setActiveTenant] = useState<string>('Juspay')
     const [activeMerchant, setActiveMerchant] =
