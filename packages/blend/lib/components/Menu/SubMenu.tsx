de4331e7aabbba07790dda31b91979a944142983
Aryan <aryan.pidiha@juspay.in>
2025-10-07 19:54:19 +0530

    fix: added border in sub-menu

--- packages/blend/lib/components/Menu/SubMenu.tsx
+++ packages/blend/lib/components/Menu/SubMenu.tsx
@@ -271,6 +271,7 @@ export const SubMenu = ({
                             ? `${maxHeight}px`
                             : 'var(--radix-popper-available-height)',
                         overflowY: 'auto',
+                        border: menuTokens.border,
                     }}
                     avoidCollisions
                     onFocusCapture={(e) => {
