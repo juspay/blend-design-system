b917ade1059e7a7787aa0478ac1cc10d68e5776c
Aryan <aryan.pidiha@juspay.in>
2025-10-16 13:53:26 +0530

    fix: minor fix

--- packages/blend/lib/components/Tabs/TabsList.tsx
+++ packages/blend/lib/components/Tabs/TabsList.tsx
@@ -174,7 +174,7 @@ const TabsList = forwardRef<HTMLDivElement, TabsListProps>(
                                         flexShrink: 0,
                                         whiteSpace: 'nowrap',
                                     }}
-                                    data-tabs={item.value}
+                                    data-tabs={item.label}
                                     data-tab-selected={item.value === activeTab}
                                     // data-tabs-disabled={item.disabled}
                                 >
