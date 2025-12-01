fa83223ee83b94d33163ae5a828bdb2fad896248
Vinit Khandal <vinit.khandal@juspay.in>
2025-12-01 18:57:26 +0530

    chore: remove unused Theme import in tag.tokens.ts

--- packages/blend/lib/components/Tags/tag.tokens.ts
+++ packages/blend/lib/components/Tags/tag.tokens.ts
@@ -4,7 +4,6 @@ import { type BreakpointType } from '../../breakpoints/breakPoints'
 import { FoundationTokenType } from '../../tokens/theme.token'
 import { getTagLightTokens } from './tag.light.tokens'
 import { getTagDarkTokens } from './tag.dark.tokens'
-import { Theme } from '../../context/ThemeProvider'
 
 /**
  * Tag Tokens following the pattern: [target].CSSProp.[size].[variant].[subType]
