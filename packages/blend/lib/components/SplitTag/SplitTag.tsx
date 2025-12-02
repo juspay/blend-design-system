076044247161cca151067d5c287ac514887e27cb
deepakoram-juspay <deepak.oram@juspay.in>
2025-12-02 15:24:41 +0530

    fix: split tag primary & secondary added

--- packages/blend/lib/components/SplitTag/SplitTag.tsx
+++ packages/blend/lib/components/SplitTag/SplitTag.tsx
@@ -16,22 +16,28 @@ const SplitTag = ({ primaryTag, secondaryTag, size, shape }: SplitTagProps) => {
                 <Tag
                     {...primaryTag}
                     splitTagPosition="left"
+                    data-element="primary-tag"
                     variant={primaryTag.variant ?? TagVariant.NO_FILL}
                     size={size}
                     shape={shape}
                     data-split-tag-left="true"
                     data-split-tag-left-text={primaryTag.text}
+                    data-status={primaryTag.color ?? ''}
+                    data-id={primaryTag.text ?? ''}
                 />
             ) : null}
             {secondaryTag ? (
                 <Tag
                     {...secondaryTag}
                     splitTagPosition="right"
+                    data-element="secondary-tag"
                     variant={secondaryTag.variant ?? TagVariant.ATTENTIVE}
                     size={size}
                     shape={shape}
                     data-split-tag-right="true"
                     data-split-tag-right-text={secondaryTag.text}
+                    data-status={secondaryTag.color ?? ''}
+                    data-id={secondaryTag.text ?? ''}
                 />
             ) : null}
         </Block>
