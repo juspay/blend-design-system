a94fb99472be463b571aadb7ae0934bad01bdbbd
Vinit Khandal <vinit.khandal@juspay.in>
2025-12-04 13:22:44 +0530

    refactor: Replace Radio with Snackbar and SidebarDemo in App

--- apps/site/src/App.tsx
+++ apps/site/src/App.tsx
@@ -1,107 +1,11 @@
-import { Heart } from 'lucide-react'
-import { Radio, RadioSize } from '../../../packages/blend/lib/main'
+import { Snackbar } from '../../../packages/blend/lib/main'
+import SidebarDemo from './demos/SidebarDemo'
 
 function App() {
     return (
-        <main className="w-screen min-h-screen bg-white flex items-center justify-center flex-col gap-4">
-            <Radio
-                size={RadioSize.MEDIUM}
-                name="radio"
-                defaultChecked={true}
-                // disabled={true}
-                // error={true}
-                // required={true}
-                subtext="This is a subtext"
-                slot={<Heart size={16} className="text-red-500" />}
-            >
-                Radio
-            </Radio>
-            <Radio
-                size={RadioSize.MEDIUM}
-                name="radio"
-                defaultChecked={true}
-                disabled={true}
-                // error={true}
-                // required={true}
-                subtext="This is a subtext"
-                slot={<Heart size={16} className="text-red-500" />}
-            >
-                Radio
-            </Radio>
-            <Radio
-                size={RadioSize.MEDIUM}
-                name="radio"
-                defaultChecked={true}
-                disabled={true}
-                error={true}
-                // required={true}
-                subtext="This is a subtext"
-                slot={<Heart size={16} className="text-red-500" />}
-            >
-                Radio
-            </Radio>
-            <Radio
-                size={RadioSize.MEDIUM}
-                name="radio"
-                defaultChecked={true}
-                // disabled={true}
-                // error={true}
-                required={true}
-                subtext="This is a subtext"
-                slot={<Heart size={16} className="text-red-500" />}
-            >
-                Radio
-            </Radio>
-            <Radio
-                size={RadioSize.MEDIUM}
-                name="radio"
-                defaultChecked={true}
-                value="radio"
-                subtext="This is a subtext"
-                slot={<Heart size={16} className="text-red-500" />}
-            >
-                Radio
-            </Radio>
-            <Radio
-                size={RadioSize.MEDIUM}
-                name="radio"
-                checked={true}
-                // required={true}
-                subtext="This is a subtext"
-                slot={<Heart size={16} className="text-red-500" />}
-            >
-                Radio
-            </Radio>
-            <Radio
-                size={RadioSize.MEDIUM}
-                name="radio"
-                checked={false}
-                // required={true}
-                subtext="This is a subtext"
-                slot={<Heart size={16} className="text-red-500" />}
-            >
-                Radio
-            </Radio>
-            <Radio
-                size={RadioSize.MEDIUM}
-                name="radio"
-                defaultChecked={true}
-                // required={true}
-                subtext="This is a subtext"
-                slot={<Heart size={16} className="text-red-500" />}
-            >
-                Radio
-            </Radio>
-            <Radio
-                size={RadioSize.MEDIUM}
-                name="radio"
-                defaultChecked={false}
-                // required={true}
-                subtext="This is a subtext"
-                slot={<Heart size={16} className="text-red-500" />}
-            >
-                Radio
-            </Radio>
+        <main className="w-screen min-h-screen bg-gray-100 flex items-center justify-center">
+            <Snackbar />
+            <SidebarDemo />
         </main>
     )
 }
