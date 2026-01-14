d71d8ec7e2a8a1fcfb4071a32ea5413084bdc71f
Vinit Khandal <vinit.khandal@juspay.in>
2026-01-14 14:05:36 +0530

    fix: Correct import statement for ButtonSubType in LinkButton tests

--- packages/blend/__tests__/components/ButtonV2/LinkButton.test.tsx
+++ packages/blend/__tests__/components/ButtonV2/LinkButton.test.tsx
@@ -6,7 +6,7 @@ import { LinkButton } from '../../../lib/components/ButtonV2'
 import {
     ButtonType,
     ButtonSize,
-    ButtonSubType
+    ButtonSubType,
 } from '../../../lib/components/ButtonV2/types'
 import { MockIcon } from '../../test-utils'
 
