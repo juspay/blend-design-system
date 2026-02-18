bdc2e7a460da9eeac23d96dd4fbb012ed2dfc1fa
Vinit Khandal <vinit.khandal@juspay.in>
2026-02-18 18:32:36 +0530

    chore: remove unused variable

--- apps/ascent/app/(home)/_components/sections/HeroSection.tsx
+++ apps/ascent/app/(home)/_components/sections/HeroSection.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { Copy, Check } from 'lucide-react'
+import { Check } from 'lucide-react'
 import Image from 'next/image'
 import { CopyIcon } from '../icons'
 
